93828a83ff9557068a7f861b5b436f8bf2a80f7b
arosxorihs <nguyentranhaiphong2004@gmail.com>
2025-09-23 17:36:39 +0700

    Update test.php

--- test.php
+++ test.php
@@ -15,5 +15,15 @@
         echo "x++: " . ($x++) . "<br/>";
         echo "++y: " . (++$y) . "<br/>";
         ?>
+        <?php
+        $x = 10;
+        $y = 11;
+        echo "x == y: " . ($x == $y) . "<br/>";
+        echo "x != y: " . ($x != $y) . "<br/>";
+        echo "x < y: " . ($x < $y) . "<br/>";
+        echo "x > y: " . ($x > $y) . "<br/>";
+        echo "x <= y: " . ($x <= $y) . "<br/>";
+        echo "x >= y: " . ($x >= $y) . "<br/>";
+        ?>
     </body>
 </html>
